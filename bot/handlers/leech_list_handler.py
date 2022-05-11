d78176511f750b24feff2fabb887c6950c5782f8
antonadel69 <105193599+antonadel69@users.noreply.github.com>
2022-05-11 14:56:09 +0200

    Add files via upload

---
+++ bot/handlers/leech_list_handler.py
@@ -0,0 +1,43 @@
+# GOAL:
+# getting track for logging
+
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+# GOAL:
+# create /list handler
+
+from os.path import join as os_path_join
+#from pyrogram import Client, Message, Filters
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from aria2p import Download
+from bot import LOCAL, STATUS, CONFIG, COMMAND
+from bot.plugins import aria2
+
+@Client.on_message(filters.command(COMMAND.LEECH_LIST))
+async def func(client: Client, message: Message):
+    dir = os_path_join(CONFIG.ROOT, CONFIG.ARIA2_DIR)
+    STATUS.ARIA2_API = STATUS.ARIA2_API or aria2.aria2(
+        config={
+            'dir' : dir
+        }
+    )
+    aria2_api = STATUS.ARIA2_API
+    await aria2_api.start()
+
+    downloads = aria2_api.get_downloads()
+    text = LOCAL.LEECH_LIST_MESSAGE_HEADER + '\n'
+    for download in downloads:
+        text += LOCAL.LEECH_LIST_FORMAT.format(
+            name = download.name,
+            status = download.status,
+            gid = download.gid
+        )
+    await message.reply(text, quote=False)
+    if message.chat.type == "private":
+        try:
+            await message.delete()
+        except:
+            pass
---
+++ bot/handlers/upload_as_doc_handler.py
@@ -0,0 +1,9 @@
+#from pyrogram import Client, Message, Filters
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from bot import LOCAL, STATUS, COMMAND
+
+@Client.on_message(filters.command(COMMAND.UPLOAD_AS_DOC))
+async def func(client : Client, message: Message):
+    STATUS.UPLOAD_AS_DOC = not STATUS.UPLOAD_AS_DOC
+    await message.reply_text(LOCAL.UPLOAD_AS_DOC.format(status=STATUS.UPLOAD_AS_DOC))
